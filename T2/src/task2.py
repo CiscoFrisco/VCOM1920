e7e2fcec5ce187d80b449dfc8f579f4df2d75951
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-05-13 22:42:12 +0100

    Improve task1 and task2

--- T2/src/task2.py
+++ T2/src/task2.py
@@ -2,36 +2,45 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+import os
+import numpy as np
+
 
 def task2():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3)))
+
+    model.add(Conv2D(16, (3, 3), input_shape=(150, 150, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_first"))
 
-    # model.add(Conv2D(32, (3, 3), input_shape=(3, 150, 150)))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_first"))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_first"))
 
-    # model.add(Conv2D(64, (3, 3), input_shape=(3, 150, 150)))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_first"))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_first"))
 
-    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    # this converts our 3D feature maps to 1D feature vectors
+    model.add(Flatten())
     model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(1))
+    model.add(Dense(7))
     model.add(Activation('sigmoid'))
 
     model.compile(loss='binary_crossentropy',
-                optimizer='rmsprop',
-                metrics=['accuracy'])
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
 
-    data_folder = '../res/ISBI2016_ISIC_Part3_Training_Data'
-    test_folder = '../res/ISBI2016_ISIC_Part3_Test_Data'
+    data_folder = '../res/Task 2/Training'
+    test_folder = '../res/Task 2/Test'
+    total_train = 7010
+    total_test = 3005
 
     batch_size = 16
+    epochs = 15
+    labels = ["Actinic keratosis", "Basal cell carcinoma", "Benign keratosis", "Dermatofibroma", "Melanocytic nevus", "Melanoma", "Vascular lesion"]
 
     # this is the augmentation configuration we will use for training
     train_datagen = ImageDataGenerator(
@@ -46,20 +55,24 @@ def task2():
 
     train_generator = train_datagen.flow_from_directory(
         data_folder, class_mode='categorical', batch_size=batch_size, target_size=(150, 150),)
-    validation_generator = test_datagen.flow_from_directory(
+    test_generator = test_datagen.flow_from_directory(
         test_folder, class_mode='categorical', batch_size=batch_size, target_size=(150, 150),)
 
     # confirm the iterator works
     batchX, batchy = train_generator.next()
     print('Batch shape=%s, min=%.3f, max=%.3f' %
-        (batchX.shape, batchX.min(), batchX.max()))
+          (batchX.shape, batchX.min(), batchX.max()))
 
     model.fit_generator(
         train_generator,
-        steps_per_epoch=2000 // batch_size,
-        epochs=5,
-        validation_data=validation_generator,
-        validation_steps=800 // batch_size)
+        steps_per_epoch=total_train // batch_size,
+        epochs=epochs)
+
+    model.evaluate_generator(test_generator)
+
+    predictions = model.predict_generator(test_generator)
+
+    print(np.argmax(predictions[0]), labels[np.argmax(predictions[0])])
+
     # always save your weights after training or during training
     model.save_weights('first_try.h5')
-
